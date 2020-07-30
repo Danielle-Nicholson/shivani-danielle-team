c4e4b39b786aa6eed5b506a1020e6fbbffc099e4
Shjani49 <shjani49@gmail.com>
2020-07-30 13:57:49 -0600

    Added new Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import About from './About';
+import abc from'./abc';
 
 function App() {
   return (
     <>
       <h1> Shivani - Danielle</h1>
       <About />
+      <abc />
+      
     </>
   
   );
---
+++ src/abc.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class abc extends React.Component{
+
+    render()
+    {
+        return(
+            <>
+            <p> Our first group project..123456789</p>
+            </>
+        );
+    }
+}
+export default abc;
