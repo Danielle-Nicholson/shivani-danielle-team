ac34831741dbdff9715e87ab1fcb1af5650862e8
Shjani49 <shjani49@gmail.com>
2020-07-30 13:37:05 -0600

    Added new component About

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component{
+
+    render()
+    {
+        return(
+            <>
+            <p> Our first group project..</p>
+            </>
+        );
+    }
+}
+export default About;
