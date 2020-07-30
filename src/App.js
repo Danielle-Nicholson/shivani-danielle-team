4514fec3218023e94aea23d8b10bd333e1e1c43d
Danielle Nicholson <daniellenicholsonart@gmail.com>
2020-07-30 13:42:43 -0600

    created things page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Things from './Things';
 import './App.css';
 
 function App() {
   return (
     <>
     <h1> Hi, this is Shivani and Danielle's test project</h1>
+    <Things/>
+    
     </>
   );
 }
